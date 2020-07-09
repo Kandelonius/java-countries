42c983241cf591afdfad560a290b0e38c3022de5
Shane Kaestner <shane.kaestner@yahoo.com>
2020-07-09 14:52:39 -0700

    testing commit

---
+++ countriestwo/src/main/java/com/lambdashane/countriestwo/CountriestwoApplication.java
@@ -0,0 +1,16 @@
+package com.lambdashane.countriestwo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CountriestwoApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(CountriestwoApplication.class,
+            args);
+    }
+
+}
